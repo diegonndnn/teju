0307b887b6f51a00674d674d48e7344ab04a7751
Diego Marques <javanatal@gmail.com>
2018-11-19 19:11:59 -0300

    Commit inicial

---
+++ src/main/java/com/diego/teju/TejuApplication.java
@@ -0,0 +1,12 @@
+package com.diego.teju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TejuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TejuApplication.class, args);
+	}
+}
